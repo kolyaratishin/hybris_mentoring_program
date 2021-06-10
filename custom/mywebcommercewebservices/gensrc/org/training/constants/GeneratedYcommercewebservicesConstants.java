d82e3287d7c74af7dec2c315b587628d148a607e
kolyaratishin <kolyaratishin1@gmail.com>
2021-06-10 16:20:57 +0300

    initial commit

---
+++ custom/mywebcommercewebservices/gensrc/org/training/constants/GeneratedYcommercewebservicesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 10 июн. 2021 г., 16:15:34                   ---
+ * ----------------------------------------------------------------
+ */
+package org.training.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYcommercewebservicesConstants
+{
+	public static final String EXTENSIONNAME = "mywebcommercewebservices";
+	
+	protected GeneratedYcommercewebservicesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
